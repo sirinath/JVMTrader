b2c88a513ac3322382d4a0f0edb80657ed4694b5
Suminda Sirinath Salpitikorala Dharmasena <sirinath1978m@gmail.com>
2015-09-23 20:12:14 +0530

    RC release for MappedBuffers

--- src/main/java/com/susico/JVMTrader/instrument/Ticker.java
+++
@@ -1,7 +0,0 @@
-package com.susico.jvmtrader.instrument;
-
-/**
- * Created by sirin_000 on 12/09/2015.
- */
-public class Ticker {
-}
--- src/main/java/com/susico/JVMTrader/offheap/SharedBuffer.java
+++
@@ -1,7 +0,0 @@
-package com.susico.jvmtrader.offheap;
-
-/**
- * Created by sirin_000 on 17/09/2015.
- */
-public class SharedBuffer {
-}
